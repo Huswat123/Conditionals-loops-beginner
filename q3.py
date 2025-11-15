e1d48a059f5f81a36572078fcfdf8a84e7588322
unknown <lhuswat@gmail.com>
2025-11-15 10:33:49 -1200

    Added third question

---
+++ q3.py
@@ -0,0 +1,21 @@
+while True:
+    score = int(input("Enter your score (0-100): "))
+    
+    if 70 <= score <= 100:
+        print("A")
+        
+    elif 60 <= score <= 69:
+        print("B")
+        
+    elif 50 <= score <= 59:
+        print("C")
+        
+    elif 40 <= score <= 49:
+        print("D")
+        
+    elif 0 <= score <= 39:
+        print("F")
+    
+    else:
+        print("Invalid score. Please try again.\n")
+    
