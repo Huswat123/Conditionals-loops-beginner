f6afa53549c39c1612839174a14e056f681df00c
unknown <lhuswat@gmail.com>
2025-11-15 11:10:30 -1200

    Added fourth question

---
+++ q4.py
@@ -0,0 +1,8 @@
+password = ""
+
+while password != "Python123":
+    password = input("Enter password: ")
+    if password != "Python123":
+        print("Wrong password, try again.\n")
+
+print("Passworld Accepted!")
