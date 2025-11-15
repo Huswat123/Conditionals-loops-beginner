4f3c5b32b906acd5f1c8486c0417601db8a8de80
unknown <lhuswat@gmail.com>
2025-11-15 11:21:15 -1200

    Esc :wq
    Removed While loop

--- q3.py
+++ q3.py
@@ -1,21 +1,31 @@
+<<<<<<< HEAD
 
     score = int(input("Enter your score (0-100): "))
+=======
+score = int(input("Enter your score (0-100): "))
+>>>>>>> 9c1b62c (Removed While loop)
     
-    if 70 <= score <= 100:
-        print("A")
+if 70 <= score <= 100:
+    print("A")
         
-    elif 60 <= score <= 69:
-        print("B")
+elif 60 <= score <= 69:
+    print("B")
         
-    elif 50 <= score <= 59:
-        print("C")
+elif 50 <= score <= 59:
+    print("C")
         
-    elif 40 <= score <= 49:
+elif 40 <= score <= 49:
         print("D")
         
-    elif 0 <= score <= 39:
-        print("F")
+elif 0 <= score <= 39:
+    print("F")
     
+<<<<<<< HEAD
     else:
         print("Invalid score. Please try again.\n")
     
+=======
+else:
+    print("Invalid score. Please try again.\n")
+    
+>>>>>>> 9c1b62c (Removed While loop)
